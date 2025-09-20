4fed480b4f7bb03cd72544b549c2a3341b681200
Prometheus <105338051+AlRzRz@users.noreply.github.com>
2025-09-20 13:21:03 +0400

    V1.0.0

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import time
+import pandas as pd
+from openai import OpenAI
+import json
 
 # GPT-like fluid text streaming into the terminal
 def streamText(text):
@@ -10,17 +13,131 @@ def streamText(text):
     print()
 
 
-
-# TODO use tool call functions to create
-
-
-
-
-
 def main():
+    df = pd.read_csv("data/transcriptions.csv")
+    df.head()
+
+    client = OpenAI()
+
+
+    medSpecialtyList = df['medical_specialty'].to_list()
+    transcriptionList = df['transcription'].to_list()
+
+
+    mainTools = [
+    {
+        "type": "function",
+        'function': {
+            "name": "get_main_medical_info",
+            "description": "Extract the 'age' and 'recommended_treatment' from each transcription",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "age": {
+                        "type": "string",
+                        "description": "The patient age extracted from the transcription.",
+                    },
+                    "recommended_treatment": {
+                        "type": "string",
+                        "description": "The recommended treatment extracted from each transcription.",
+                    },
+                },
+            },
+        },
+    },
+    ]
+
+    transcriptionContext = '||'.join(transcriptionList)
+    print(transcriptionContext)
+
+    messages=[
+        {'role': 'system', 'content': 'You are a medical transcription agent. You extract medical information from these transcripts and automate the matching with the appropriate ICD-10 codes. ICD-10 codes are a standardized system used worldwide for diagnosing and billing purposes, such as insurance claims processing.'}
+    ]
+
+    messages.append({
+        'role': 'user',
+        'content': f"""
+        These are medical transcriptions separated with a '||' in proper order. Apply the appropriate tools to complete your function please.
+        
+        {transcriptionContext}"""
+    })
+
+    response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=messages,
+    tools=mainTools
     
-    streamText("How can I help you today?")
+)
 
+    print(response)
 
-if __name__ == '__main__':
-    main()
+    print(response.choices[0].message.tool_calls)
+
+    listOfMainInfo = response.choices[0].message.tool_calls
+
+    finalDf = pd.DataFrame()
+    finalDf['medical_specialty'] = df['medical_specialty']
+    print(finalDf.head())
+
+    mainInfoListOfDicts = [json.loads(resp.function.arguments) for resp in listOfMainInfo]
+    print(mainInfoListOfDicts)
+
+    intermediaryDf = pd.DataFrame(mainInfoListOfDicts)
+    print(intermediaryDf)
+
+    finalDf['age'] = intermediaryDf['age']
+    finalDf['recommended_treatment'] = intermediaryDf['recommended_treatment']
+
+    lstOfRecommendedTreatments = finalDf['recommended_treatment'].values.tolist()
+
+    ICDTools = [
+    {
+        "type": "function",
+        'function': {
+            "name": "get_icd_code",
+            "description": "Match each recommended treatment with the corresponding International Classification of Diseases (ICD) code",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "icd_code": {
+                        "type": "string",
+                        "description": "An ICD-10 code which is a standardized system used worldwide for diagnosing and billing purposes, such as insurance claims processing.",
+                    },
+                },
+            },
+        },
+    },
+    ]
+
+    ICDmessages=[
+            {'role': 'system', 'content': 'You are a medical transcription agent. You match recommended medical treatments from these patient\'s recommended treatments match them with the appropriate ICD-10 codes. ICD-10 codes are a standardized system used worldwide for diagnosing and billing purposes, such as insurance claims processing.'}
+        ]
+
+    ICDmessages.append({
+        'role': 'user',
+        'content': f"""
+        These are recommended medical treatments separated with a '||' in proper order. Apply the appropriate tools to complete your function please.
+        
+        {lstOfRecommendedTreatments}"""
+    })
+
+    ICDresponse = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages=ICDmessages,
+    tools=ICDTools
+    
+    )
+
+    print(ICDresponse.choices[0].message.tool_calls)
+
+    icds = ICDresponse.choices[0].message.tool_calls
+
+    lstOfIcds = [json.loads(icd.function.arguments) for icd in icds]
+    print(lstOfIcds)
+
+    intermDf = pd.DataFrame(lstOfIcds)
+
+    finalDf['icd_code'] = intermDf['code']
+
+    df_structured = finalDf
+    print(df_structured)
